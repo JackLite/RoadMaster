3bc00db0260591ba965ad1be04d1ef294e3f2f1f
Сергей Дючков <jackkdlite@gmail.com>
2019-11-22 18:32:13 +0300

    Тестирую ECS

--- Assets/Scripts/RoadCreator.cs
+++ Assets/Scripts/RoadCreator.cs
@@ -1,35 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public class RoadCreator : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _roadPrefab;
-
+    public GameObject roadPrefab;
     void Start()
     {
-        foreach(Transform child in transform)
-        {
-            Destroy(child.gameObject);
-        }
-        for(var i = 0; i < 11; i++)
-        {
-            AddRoad(new Cell() { X = i, Y = 0 });
-            AddRoad(new Cell() { X = i, Y = 10 });
-        }
-
-        for (var i = 1; i < 10; i++)
-        {
-            AddRoad(new Cell() { X = 0, Y = i });
-            AddRoad(new Cell() { X = 10, Y = i });
-        }
-
-    }
-
-    private void AddRoad(Cell cell)
-    {
-        var position = new Vector2(cell.X + 0.5f, cell.Y + 0.5f);
-        Instantiate(_roadPrefab, position, Quaternion.identity, transform);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(roadPrefab, World.Active);
+        var manager = World.Active.EntityManager;
+        var entity = manager.Instantiate(prefab);
     }
 }
